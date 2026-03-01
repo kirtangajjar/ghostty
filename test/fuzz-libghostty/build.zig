849cf7139e3e74c3a24c292de13b3c1f4ce2b090
Mitchell Hashimoto <m@mitchellh.com>
2026-02-28 20:17:52 -0800

    test/fuzz-libghostty: add zig build run
    Executed-By: ghostty/polecats/fury
    Rig: ghostty
    Role: polecats

--- test/fuzz-libghostty/build.zig
+++ test/fuzz-libghostty/build.zig
@@ -4,6 +4,7 @@ const afl = @import("afl");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const run_step = b.step("run", "Run the fuzzer with afl-fuzz");
 
     // Create the C ABI library from Zig source that exports the
     // API that the `afl-cc` main.c entrypoint can call into. This
@@ -42,8 +43,27 @@ pub fn build(b: *std.Build) void {
     // static Zig library. afl-cc is expecte to be on the PATH.
     const exe = afl.addInstrumentedExe(b, lib);
 
+    // Runner to simplify running afl-fuzz
+    const run = run: {
+        const run = b.addSystemCommand(&.{
+            b.findProgram(&.{"afl-fuzz"}, &.{}) catch
+                @panic("Could not find 'afl-fuzz', which is required to run"),
+            "-i",
+        });
+        run.addDirectoryArg(b.path("corpus/initial"));
+        run.addArgs(&.{"-o"});
+        run.addDirectoryArg(b.path("afl-out"));
+        run.addArgs(&.{"--"});
+        run.addFileArg(exe);
+        run.addArgs(&.{"@@"});
+        break :run run;
+    };
+
     // Install
     b.installArtifact(lib);
     const exe_install = b.addInstallBinFile(exe, "ghostty-fuzz");
     b.getInstallStep().dependOn(&exe_install.step);
+
+    // Run
+    run_step.dependOn(&run.step);
 }
